8ae3288103e702e3d9bf1328a20f4ab3652cce67
GuilloOme <guillaume@piiou.net>
2016-01-26 20:11:33 -0500

    wip

--- HectorDuino.ino
+++ HectorDuino.ino
@@ -1,4 +1,6 @@
-#include <WebServer.h>
+#include "SPI.h"
+#include "Ethernet.h"
+#include "WebServer.h"
 
 /*
   Blink
@@ -9,24 +11,77 @@
  
  
 /*
- * MAC Address
+ * MAC Address and IP
  */
 static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x87, 0xFE };
+static uint8_t ip[] = { 192, 168, 88, 88 };
 
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 12;
 
+
+/* This creates an instance of the webserver.  By specifying a prefix
+ * of "", all pages will be at the root of the server. */
+#define PREFIX "/light"
+WebServer webserver(PREFIX, 80);
+
+/* commands are functions that get called by the webserver framework
+ * they can read any posted data from client, and they output to the
+ * server to send data back to the web browser. */
+void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
+{
+  /* this line sends the standard "we're all OK" headers back to the
+     browser */
+  server.httpSuccess();
+
+  /* if we're handling a GET or POST, we can output our data here.
+     For a HEAD request, we just stop after outputting headers. */
+  if (type != WebServer::HEAD)
+  {
+    /* this defines some HTML text in read-only memory aka PROGMEM.
+     * This is needed to avoid having the string copied to our limited
+     * amount of RAM. */
+    P(helloMsg) = "<h1>Hello, World!</h1>";
+      
+
+
+    /* this is a special form of print that outputs from PROGMEM */
+    server.printP(helloMsg);
+  }
+}
+
+
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
+  
+  /* initialize the Ethernet adapter */
+  Ethernet.begin(mac, ip);
+
+  /* setup our default command that will be run when the user accesses
+   * the root page on the server */
+  webserver.setDefaultCommand(&helloCmd);
+
+  /* run the same command if you try to load /index.html, a common
+   * default page name */
+  webserver.addCommand("",&helloCmd);
+
+  /* start the webserver */
+  webserver.begin();
+  
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+
+  /* process incoming connections one at a time forever */
+  webserver.processConnection();
+  
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a second
+  delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);               // wait for a second
+  delay(1000);               // wait for a second
+
 }
